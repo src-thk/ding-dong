4907ae58c9a65526f60f6d4f0563ec8ba36f5f01
Sarthak Tandon <Sarthak-Tandon@outlook.com>
2025-05-22 01:07:45 +0530

    refactor: migrate to TanStack Router and restructure routing
    This commit introduces TanStack Router for handling routing in the application. It removes the old `App.tsx` and `index.tsx` files and replaces them with a new routing structure using `__root.tsx`, `index.tsx`, and `about.tsx`. Additionally, the `rsbuild.config.ts` has been updated to support the new routing setup, and necessary dependencies have been added to `package.json`.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
 
 export default defineConfig({
+  source: {
+    entry: {
+      index: './src/main',
+    },
+  },
   plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      plugins: [
+        TanStackRouterRspack({
+          target: 'react',
+          autoCodeSplitting: true,
+          routesDirectory: './src/routes',
+          generatedRouteTree: './src/routeTree.gen.ts',
+          routeFileIgnorePrefix: '-',
+          quoteStyle: 'single'
+        }),
+      ],
+    },
+  },
 });
---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+import { routeTree } from '@/routeTree.gen'
+
+import '@/styles/globals.css'
+
+const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </StrictMode>,
+    )
+}
